b1802356ca90c63cb88af762880110747541ed41
MANGLAM DUBEY <manglamdubey11@gmail.com>
2024-10-04 22:00:50 +0530

    completed the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -20,28 +20,38 @@ const Navbar = () => {
 
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src={logo} className="h-8" alt="Flashcard Logo" />
+            <div className="max-w-screen-full flex flex-wrap items-center justify-between mx-auto p-4">
+                <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse ml-4">
+                    <img src={logo} className="h-12 w-12 rounded-xl" alt="Flashcard Logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flashcard App</span>
                 </Link>
                 <div className="flex items-center md:order-2">
                     {userLoggedIn ? (
                         <button
                             type="button"
-                            className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                            className="flex text-lg bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
                             onClick={() => setIsUserDropdownOpen(!isUserDropdownOpen)}
                         >
                             <span className="sr-only">Open user menu</span>
                             <img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="user photo" />
                         </button>
                     ) : (
-                        <Link to="/login" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Login</Link>
+                        <NavLink
+                            to="/login"
+                            className={({ isActive }) =>
+                                `text-lg font-medium rounded-lg px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 ${isActive
+                                    ? "text-blue-800 dark:text-blue-400"
+                                    : "text-gray-800 dark:text-white"
+                                } hover:bg-blue-50 focus:ring-4 focus:ring-blue-300 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-blue-700`
+                            }
+                        >
+                            Login
+                        </NavLink>
                     )}
                     <button
                         data-collapse-toggle="navbar-user"
                         type="button"
-                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-lg text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                         onClick={() => setIsMenuOpen(!isMenuOpen)}
                     >
                         <span className="sr-only">Open main menu</span>
@@ -51,7 +61,7 @@ const Navbar = () => {
                     </button>
                 </div>
                 <div className={`items-center justify-between ${isMenuOpen ? 'block' : 'hidden'} w-full md:flex md:w-auto md:order-1`} id="navbar-user">
-                    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <ul className="flex flex-col font-medium text-lg p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
                             <NavLink
                                 to="/"
@@ -105,24 +115,24 @@ const Navbar = () => {
                 {isUserDropdownOpen && userLoggedIn && (
                     <div className="z-50 absolute right-0 mt-2 w-48 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
                         <div className="px-4 py-3">
-                            <span className="block text-sm text-gray-900 dark:text-white">{currentUser.displayName}</span>
-                            <span className="block text-sm text-gray-500 truncate dark:text-gray-400">{currentUser.email}</span>
+                            <span className="block text-lg text-gray-900 dark:text-white">{currentUser.displayName}</span>
+                            <span className="block text-lg text-gray-500 truncate dark:text-gray-400">{currentUser.email}</span>
                         </div>
                         <ul className="py-2" aria-labelledby="user-menu-button">
                             <li>
-                                <Link to="/profile" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profile</Link>
+                                <Link to="/profile" className="block px-4 py-2 text-lg text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profile</Link>
                             </li>
                             <li>
-                                <Link to="/settings" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</Link>
+                                <Link to="/settings" className="block px-4 py-2 text-lg text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</Link>
                             </li>
                             <li>
-                                <button onClick={handleLogout} className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</button>
+                                <button onClick={handleLogout} className="block w-full text-left px-4 py-2 text-lg text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</button>
                             </li>
                         </ul>
                     </div>
                 )}
             </div>
-        </nav>
+        </nav >
     );
 };
 
